e53836e826506c6cfddc8a28bc6978ed27eaef63
Jonathan Wamarema <jonathan@databit.net>
2023-03-20 14:59:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("Programming is like building a multilingual puzzle");
     return (0);
 }
