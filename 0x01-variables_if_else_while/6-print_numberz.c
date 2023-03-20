101c74b2c79e392a33e5bd5417eaa651738c0811
Jonathan Wamarema <jonathan@databit.net>
2023-03-20 16:36:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n % 10 + '0');
+	putchar('\n');
+
+	return (0);
+}
