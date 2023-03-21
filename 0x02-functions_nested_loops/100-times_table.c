8ad6e61279612e553ff8b9434cfdc2229368b502
Jonathan Wamarema <jonathan@databit.net>
2023-03-21 14:47:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,29 +4,22 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
+  if (n < 0 || n > 15) {
+    return;
+  }
 
-	for (int i = 0; i <= n; i++)
-	{
-		for (int j = 0; j <= n; j++)
-		{
-			int result = i * j;
+  int i, j, product;
 
-			if (j == 0)
-				putchar('0');
-			else if (result < 10)
-				putchar(' '), putchar(' '), putchar(result + '0');
-			else if (result < 100)
-				putchar(' '), putchar(result / 10 + '0'), putchar(result % 10 + '0');
-			else
-				putchar(result / 100 + '0'), putchar(result % 100 / 10 + '0'), putchar(result % 10 + '0');
-
-			if (j < n)
-				putchar(','), putchar(' ');
-			else
-				putchar('\n');
-		}
-	}
+  for (i = 0; i <= n; i++) {
+    for (j = 0; j <= n; j++) {
+      product = i * j;
+      if (j == 0) {
+        printf("%d", product);
+      } else {
+        printf(",%3d", product);
+      }
+    }
+    printf("\n");
+  }
 }
 
