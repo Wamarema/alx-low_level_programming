aaf33771dc9d9ca309b2fd38e041130f5b02d662
Jonathan Wamarema <jonathan@databit.net>
2023-03-28 15:42:05 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout followed by a new line
+ *
+ * @str: the string to print
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (*(str + i) != '\0')
+    {
+        _putchar(*(str + i));
+        i++;
+    }
+    _putchar('\n');
+}
+
